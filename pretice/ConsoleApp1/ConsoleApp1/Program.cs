20bd91d5fca381aa276bff68c25fd489540c8a5e
CrystalAlfred <98390215+CrystalAlfred@users.noreply.github.com>
2025-09-15 14:49:38 +0800

    Add files via upload

---
+++ pretice/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+class Program
+{
+    static void Main (string[] args)
+{
+        Console.WriteLine("<<<兩位數相加>>>");
+        string input1,input2;
+        int num1,num2;
+        Console.Write("數字一:");
+        input1 = Console.ReadLine();
+        num1=Convert.ToInt32 (input1);
+        Console.Write("數字二:");
+        input2 = Console.ReadLine();
+        num2 = Convert.ToInt32(input2);
+        int result=num1 + num2;
+        Console.Write(num1);
+        Console.Write("+");
+        Console.Write(num2);
+        Console.Write("=");
+        Console.Write(result+"\n");
+        Console.WriteLine(num1 +"+"+ num2+"="+result);
+        Console.WriteLine("{0}+{1}+{2}",num1,num2,result);
+
+    }
+}
